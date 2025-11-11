2c41f6d20980de93dd9956e9d280a878a6fbe53b
jenelleandderson123 <jenelleannderson@gmail.com>
2025-11-11 19:12:10 +0000

    Add files via upload

---
+++ agee.py
@@ -0,0 +1,9 @@
+age = int(input("Enter your age: "))
+
+if age >= 10:
+    if age <= 20:
+        print("Yup, your age is between 10 and 20. Congrats, you're in the discount teenage range.")
+    else:
+        print("You're above 20. Relax old head.")
+else:
+    print("You're below 10. Go drink Milo and sleep.")
